1dadb317e09c60cfb21610ccdd8061c888240354
Micky D. <iiz10ninja@gmail.com>
2025-05-06 17:26:31 -0500

    move ytdlp plugin outside

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,50 +1,28 @@
-import type { Options } from "tsup";
+/* eslint-disable linebreak-style */
 import { defineConfig } from "tsup";
 
-export function createTsupConfig({
-  entry = ["src/index.ts"],
-  external = [],
-  noExternal = [],
-  platform = "node",
-  format = ["esm", "cjs"],
-  target = "es2022",
-  skipNodeModulesBundle = true,
-  clean = true,
-  shims = format.includes("cjs"),
-  cjsInterop = format.includes("cjs"),
-  minify = false,
-  terserOptions = {
+export default defineConfig({
+  entry: ["src/index.ts"],
+  external: [],
+  noExternal: [],
+  platform: "node",
+  format: ["esm", "cjs"],
+  target: "es2022",
+  skipNodeModulesBundle: true,
+  clean: true,
+  shims: true,
+  cjsInterop: true,
+  minify: false,
+  terserOptions: {
     mangle: false,
     keep_classnames: true,
     keep_fnames: true,
   },
-  splitting = false,
-  keepNames = true,
-  dts = true,
-  sourcemap = true,
-  esbuildPlugins = [],
-  treeshake = false,
-  outDir = "dist",
-}: Options = {}) {
-  return defineConfig({
-    entry,
-    external,
-    noExternal,
-    platform,
-    format,
-    skipNodeModulesBundle,
-    target,
-    clean,
-    shims,
-    cjsInterop,
-    minify,
-    terserOptions,
-    splitting,
-    keepNames,
-    dts,
-    sourcemap,
-    esbuildPlugins,
-    treeshake,
-    outDir,
-  });
-}
+  splitting: false,
+  keepNames: true,
+  dts: true,
+  sourcemap: true,
+  esbuildPlugins: [],
+  treeshake: false,
+  outDir: "dist",
+});
